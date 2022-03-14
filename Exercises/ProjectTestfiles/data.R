28e8ae941ff94e8b17213c3b59f9c0f141f68d7a
Brad McNeney <mcneney@sfu.ca>
2022-03-14 15:37:52 -0700

    add project testfiles

---
+++ Exercises/ProjectTestfiles/data.R
@@ -0,0 +1,12 @@
+#' A test dataset for the mars package.
+#' 
+#' A dataset of size N=100 with n=10 explanatory variables, 
+#' and a response variable that depends on only the first two 
+#' explanatory variables.
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{y}{response variable}
+#'   \item{x1,...,x10}{explanatory variables}
+#' }
+"marstestdata"
