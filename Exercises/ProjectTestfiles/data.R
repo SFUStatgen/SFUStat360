83aea2a6e1502893681d7b29c887e268560a2a36
Brad McNeney <mcneney@sfu.ca>
2022-03-15 12:17:15 -0700

    add explanatory var doc

--- Exercises/ProjectTestfiles/data.R
+++ Exercises/ProjectTestfiles/data.R
@@ -7,6 +7,15 @@
 #' @format A data frame with 100 rows and 11 variables:
 #' \describe{
 #'   \item{y}{response variable}
-#'   \item{x1,...,x10}{explanatory variables}
+#'   \item{x1}{explanatory variable}
+#'   \item{x2}{explanatory variable}
+#'   \item{x3}{explanatory variable}
+#'   \item{x4}{explanatory variable}
+#'   \item{x5}{explanatory variable}
+#'   \item{x6}{explanatory variable}
+#'   \item{x7}{explanatory variable}
+#'   \item{x8}{explanatory variable}
+#'   \item{x9}{explanatory variable}
+#'   \item{x10}{explanatory variable}
 #' }
 "marstestdata"
