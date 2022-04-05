ea90f1bd8a9374af676ec0b4dd0df4c4e426d574
Brad McNeney <mcneney@sfu.ca>
2022-04-05 15:17:17 -0700

    Update testpredict.R

--- Exercises/ProjectTestfiles/testthat/testpredict.R
+++ Exercises/ProjectTestfiles/testthat/testpredict.R
@@ -1,6 +1,6 @@
 library(mars)
 load("testpredict.RData")
 test_that("predict.mars() returns the correct predictions on the same data used to fit the model", {
-  expect_equal(predictmars(testmars), testpredict)} )
+  expect_equal(predict.mars(testmars), testpredict)} )
 test_that("predict.mars() returns the correct predictions on new data", {
-  expect_equal(predictmars(testmars,newdata=marstestdata), testpredict)} )
+  expect_equal(predict.mars(testmars,newdata=marstestdata), testpredict)} )
