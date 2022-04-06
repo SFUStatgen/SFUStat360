861c57387f779906cbba57f2846ff15ab34ad395
Brad McNeney <mcneney@sfu.ca>
2022-04-05 21:04:42 -0700

    put data.R back

---
+++ Exercises/ProjectTestfiles/data.R
@@ -0,0 +1,21 @@
+#' A test dataset for the mars package.
+#'
+#' A dataset of size N=100 with n=10 explanatory variables,
+#' and a response variable that depends on only the first two
+#' explanatory variables.
+#'
+#' @format A data frame with 100 rows and 11 variables:
+#' \describe{
+#'   \item{y}{response variable}
+#'   \item{x1}{explanatory variable}
+#'   \item{x2}{explanatory variable}
+#'   \item{x3}{explanatory variable}
+#'   \item{x4}{explanatory variable}
+#'   \item{x5}{explanatory variable}
+#'   \item{x6}{explanatory variable}
+#'   \item{x7}{explanatory variable}
+#'   \item{x8}{explanatory variable}
+#'   \item{x9}{explanatory variable}
+#'   \item{x10}{explanatory variable}
+#' }
+"marstestdata"
